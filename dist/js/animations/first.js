cfb6669214253c4f1729fad2314ae43c15f382f3
SimoneRocutto <76948023+SimoneRocutto@users.noreply.github.com>
2021-06-10 10:20:24 +0200

    elevator landing page animation, index.php divided into sections

--- dist/js/animations/first.js
+++ dist/js/animations/first.js
@@ -8,19 +8,21 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 import { timeout } from '../functions/index.js';
-const homeWrapperBefore = document.getElementById("home-wrapper");
+const landingWrapperBefore = document.getElementById("landing-wrapper");
 const navbar = document.getElementById("navbar");
-const homeCta = document.getElementById("home-cta");
+const landingCta = document.getElementById("landing-cta");
+const landingTitle = document.getElementById("landing-title");
 const title = document.getElementById("intro-title");
 const firstParagraph = document.getElementById("first-intro-paragraph");
 const secondParagraph = document.getElementById("second-intro-paragraph");
 const downArrow = document.getElementById("second-slide__btn-box");
-homeCta === null || homeCta === void 0 ? void 0 : homeCta.addEventListener("click", () => __awaiter(void 0, void 0, void 0, function* () {
+landingCta === null || landingCta === void 0 ? void 0 : landingCta.addEventListener("click", () => __awaiter(void 0, void 0, void 0, function* () {
     // this if statement avoids the useless wait if the animation has already happened once
     if (navbar === null || navbar === void 0 ? void 0 : navbar.classList.contains("hidden")) {
-        homeWrapperBefore === null || homeWrapperBefore === void 0 ? void 0 : homeWrapperBefore.classList.add("full-size");
+        landingWrapperBefore === null || landingWrapperBefore === void 0 ? void 0 : landingWrapperBefore.classList.add("full-size");
         navbar === null || navbar === void 0 ? void 0 : navbar.classList.remove("hidden");
         yield timeout(700);
+        landingTitle === null || landingTitle === void 0 ? void 0 : landingTitle.classList.add("hidden");
     }
     window.scroll(0, window.innerHeight);
     title === null || title === void 0 ? void 0 : title.classList.remove("slided-out");
--- src/typescript/animations/first.ts
+++ src/typescript/animations/first.ts
@@ -1,19 +1,21 @@
 import { timeout } from '../functions/index.js'
 
-const homeWrapperBefore = document.getElementById("home-wrapper")
+const landingWrapperBefore = document.getElementById("landing-wrapper")
 const navbar = document.getElementById("navbar")
-const homeCta = document.getElementById("home-cta")
+const landingCta = document.getElementById("landing-cta")
+const landingTitle = document.getElementById("landing-title")
 const title = document.getElementById("intro-title")
 const firstParagraph = document.getElementById("first-intro-paragraph")
 const secondParagraph = document.getElementById("second-intro-paragraph")
 const downArrow = document.getElementById("second-slide__btn-box")
 
-homeCta?.addEventListener("click", async () => {
+landingCta?.addEventListener("click", async () => {
     // this if statement avoids the useless wait if the animation has already happened once
     if (navbar?.classList.contains("hidden")) {
-        homeWrapperBefore?.classList.add("full-size")
+        landingWrapperBefore?.classList.add("full-size")
         navbar?.classList.remove("hidden")
         await timeout(700)
+        landingTitle?.classList.add("hidden")
     }
     window.scroll(0, window.innerHeight)
     title?.classList.remove("slided-out")
