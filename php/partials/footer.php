e00df3c0bb5e40e730f79e3db4d358a277e6d991
SimoneRocutto <76948023+SimoneRocutto@users.noreply.github.com>
2021-06-16 11:55:05 +0200

    refactor: php files moved out of the dist folder

--- php/partials/footer.php
+++ php/partials/footer.php
@@ -2,7 +2,7 @@
 <footer class="footer">
     <div class="copyright-text">2021, Designed & Developed by Simone Rocutto.</div>
 </footer>
-<script src="../main.js" type="module"></script>
+<script src="../../dist/main.js" type="module"></script>
 </body>
 
 </html>
