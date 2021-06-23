5b0b90bde415dde0c150b0688901e278e4e03812
SimoneRocutto <76948023+SimoneRocutto@users.noreply.github.com>
2021-06-23 15:24:33 +0200

    .gitignore updated; assets folder created in public folder

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './resources/assets/js/main.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'public')
+        path: path.resolve(__dirname, 'public/assets')
     },
     module: {
         rules: [
